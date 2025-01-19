4764981ef308d4a26eae1430738cd35706d39373
houda <houda.elabbassi@stud.fra-uas.de>
2025-01-19 20:26:16 +0100

    update

--- main.py
+++ main.py
@@ -17,10 +17,16 @@ async def say_hello(name: str):
 
 
 @app.get("/checkMailLLM")
-async def check_mail(api_key, endpoint, deployment_name, prompt, email):
+async def check_mail(api_key, endpoint, deployment_name, prompt, betreff, email):
+    #betreff prüfen
+    #email extract urls and see if they are phishing
+    #inhalt email auf phishing überprüfen
+    #output: ["betreff":{ "grund":"blabla", "score":1}, "email":{ "grund":"blabla", "score":1}, "url":{ "grund":"blabla", "score":1}]
+    # betreff: 0.1, email: 0.2, url: 0.1 => gesamt=> 2*scB, 4*scE, 4*scU /10 = 0.14 < 0.5 => kein Phishing (Gewichtung stimmt nicht)
+    # betreff: 0.6, email: 0.7, url: 0.8 => gesamt=> scB, 4*scE, 5*scU /10 = 0.7 > 0.5 => Phishing
     return EmailClassifier(api_key, endpoint, deployment_name, prompt, email).main(api_key, endpoint, deployment_name, prompt, email)
 
 
 @app.get("/checkMailLoRa")
 async def check_mail_lora(mail: str, betreff: str):
-    return LocalModelEmailClassifier("/Users/houda/Downloads/lora_finetuned_model/").main()
+    return LocalModelEmailClassifier("./lora_finetuned_model").main()
